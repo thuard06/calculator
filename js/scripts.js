1a4c8e2272877ee3d3bbbb5211e3030a2cd1b863
Ty Huard <thuard06@gmail.com>
2020-06-23 20:46:42 -0700

    Add Farenheit to Celcius function

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,6 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
-alert(result);
-
-
 function subtract(number1, number2) {
   return number1 - number2;
 }
@@ -22,7 +16,18 @@ function divide(number1, number2) {
   return number1 / number2;
 }
 
+function toCelsius(fahrenheit) {
+  return (5/9) * (fahrenheit-32);
+}
 
-const result = divide(number1, number2);
-alert(result);
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+const farenheit = parseInt(prompt("What is the temperarture in Farenheit?"))
 
+const addResult = add(number1, number2);
+const subtractResult = subtract(number1, number2);
+const divideResult = divide(number1, number2);
+const multiplyResult = multiply(number1, number2);
+
+const result = add(number1, number2);
+alert(toCelsius);
